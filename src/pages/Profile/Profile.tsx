3a4a866bea7d082e0a8d59c4641c0f3435a2e046
Pedram <Pedramkhodadad0@gmail.com>
2026-08-17 20:16:12 +0330

    Fix Profile page responsive

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -4,12 +4,12 @@ import ProfileSidebar from "../../components/ProfileSidebar/ProfileSidebar";
 function Profile() {
   return (
     <div>
-      <aside className="pl-4 pt-24">
+      <aside className="pl-4 pt-24 hidden lg:block">
         <ProfileSidebar />
       </aside>
       <main className="w-full">
         <div className="mx-auto flex flex-col gap-8 px-4">
-          <div className="flex justify-center pl-80">
+          <div className="flex justify-center lg:pl-80">
             <ProfileCard />
           </div>
         </div>
