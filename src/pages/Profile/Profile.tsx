94ae2e2e824ef05430e90f6a2fbf1d6888bb4e68
Zahra Kari <zahrakri0203@gmail.com>
2026-08-19 23:54:30 +0330

    feat(profile): add responsive posts and likes tabs with theme support

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,16 +1,27 @@
 import ProfileCard from "../../components/Profile/ProfileCard/ProfileCard";
+import ProfilePosts from "../../components/Profile/ProfilePosts/ProfilePosts";
 import ProfileSidebar from "../../components/ProfileSidebar/ProfileSidebar";
 
 function Profile() {
+  const isOwnProfile = true;
+
   return (
-    <div>
-      <aside className="pl-4 pt-24 hidden lg:block">
+    <div className="min-h-screen">
+      {/* Desktop Sidebar */}
+      <aside className="hidden pl-4 pt-24 lg:block">
         <ProfileSidebar />
       </aside>
-      <main className="w-full">
-        <div className="mx-auto flex flex-col gap-8 px-4">
-          <div className="flex justify-center lg:pl-80">
+
+      {/* Main Content */}
+      <main className="w-full px-4 pb-10 lg:pl-80  pt-24 lg:pt-0">
+        <div className="mx-auto flex w-full max-w-[840px] flex-col gap-4 items-center">
+          <div className="w-full max-w-[524px]">
             <ProfileCard />
+          </div>
+
+          {/* Posts / Likes */}
+          <div className="mt-6 w-full">
+            <ProfilePosts isOwnProfile={isOwnProfile} />
           </div>
         </div>
       </main>
