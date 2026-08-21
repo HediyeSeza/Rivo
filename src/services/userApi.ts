c83261bb52a2fdf788f856ec6075c4cdd044d6cc
Pedram <Pedramkhodadad0@gmail.com>
2026-08-21 23:34:32 +0330

    connect api to recommended follow sidebar

---
+++ src/services/userApi.ts
@@ -0,0 +1,16 @@
+import { api } from "./api";
+import type { User } from "../types/user";
+
+interface RecommendedUsersResponse {
+  message: string;
+  success: boolean;
+  data: User[];
+}
+
+export const getRecommendedUsers = async (): Promise<User[]> => {
+  const response = await api.get<RecommendedUsersResponse>(
+    "/api/users/recommend",
+  );
+
+  return response.data;
+};
