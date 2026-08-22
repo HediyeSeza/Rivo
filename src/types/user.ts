a50450b282bddd0a57c2612d14aad1d4a8e21d46
Zahra Kari <zahrakri0203@gmail.com>
2026-08-22 07:27:08 +0330

    feat(profile): extend user type

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,10 +1,27 @@
 export interface User {
   id: string | number;
+
   name: string;
   email: string;
   username?: string;
+
   avatar?: string;
+
+  bio?: string;
+  location?: string;
+  website?: string;
+
+  postsCount?: number;
+  followersCount?: number;
+  followingCount?: number;
+
+  isFollowing?: boolean;
 }
+
+export type UpdateProfilePayload = Pick<
+  User,
+  "name" | "bio" | "location" | "website"
+>;
 
 export interface AuthResponse {
   user?: User;
